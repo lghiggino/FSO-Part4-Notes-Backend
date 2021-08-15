65d7129d1cbe8d48cb36166950c2e0ef9bb2229f
lghiggino <lghiggino@gmail.com>
2021-08-15 20:12:48 -0300

    users work, but had to be removed due to double connection

--- app.js
+++ app.js
@@ -2,23 +2,15 @@ const config = require("./utils/config")
 const express = require("express")
 const app = express()
 const cors = require("cors")
-// const notesRouter = require("./controllers/notesController")
-const usersRouter = require("./controllers/usersController")
+const notesRouter = require("./controllers/notesController")
+// const usersRouter = require("./controllers/usersController")
 const middleware = require("./utils/middleware")
 const logger = require("./utils/logger")
 const mongoose = require("mongoose")
 
 logger.info(`connecting to ${config.MONGODB_URI} ...`)
 
-// mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-//     .then(() => {
-//         logger.info("Connected to MongoDB")
-//     })
-//     .catch(error => {
-//         logger.error("error connecting to MongoDB:", error.message)
-//     })
-
-mongoose.connect(config.MONGODB_USERS_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
     .then(() => {
         logger.info("Connected to MongoDB")
     })
@@ -26,13 +18,21 @@ mongoose.connect(config.MONGODB_USERS_URI, { useNewUrlParser: true, useUnifiedTo
         logger.error("error connecting to MongoDB:", error.message)
     })
 
+// mongoose.connect(config.MONGODB_USERS_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+//     .then(() => {
+//         logger.info("Connected to MongoDB")
+//     })
+//     .catch(error => {
+//         logger.error("error connecting to MongoDB:", error.message)
+//     })
+
 app.use(cors())
 app.use(express.static("build"))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
-// app.use("/api/notes", notesRouter)
-app.use("/api/users", usersRouter)
+app.use("/api/notes", notesRouter)
+// app.use("/api/users", usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
