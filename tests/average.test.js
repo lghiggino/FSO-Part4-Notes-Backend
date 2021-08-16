509e8b8cbae3e9a4dba2627f17e07d2297a57379
lghiggino <lghiggino@gmail.com>
2021-08-15 22:01:04 -0300

    average test

--- tests/average.test.js
+++ tests/average.test.js
@@ -1,16 +1,23 @@
 const average = require("../utils/for_testing").average
 
-test("average of [2, 2]", () => {
-    const result = average([2, 2])
-    expect(result).toBe(2)
-})
+describe(average, () => {
+    it("average of [2, 2]", () => {
+        const result = average([2, 2])
+        expect(result).toBe(2)
+    })
 
-test("average of [2, 2, 3]", () => {
-    const result = average([2, 2, 3])
-    expect(result).toBe(2.3333333333333335)
-})
+    it("average of [2, 2, 3]", () => {
+        const result = average([2, 2, 3])
+        expect(result).toBe(2.3333333333333335)
+    })
 
-test("average of [1]", () => {
-    const result = average([1])
-    expect(result).toBe(1)
+    it("average of [1]", () => {
+        const result = average([1])
+        expect(result).toBe(1)
+    })
+
+    it.only("average of empty array: [] is zero 0", () => {
+        const result = average([])
+        expect(result).toBe(0)
+    })
 })
--- utils/for_testing.js
+++ utils/for_testing.js
@@ -1,19 +1,21 @@
 const palindrome = (string) => {
     return string
-      .split('')
-      .reverse()
-      .join('')
-  }
-  
-  const average = (array) => {
+        .split('')
+        .reverse()
+        .join('')
+}
+
+const average = (array) => {
     const reducer = (sum, item) => {
-      return sum + item
+        return sum + item
     }
-  
-    return array.reduce(reducer, 0) / array.length
-  }
-  
-  module.exports = {
+
+    return array.length === 0 
+    ? 0 
+    : array.reduce(reducer, 0) / array.length
+}
+
+module.exports = {
     palindrome,
     average,
-  }
+}
