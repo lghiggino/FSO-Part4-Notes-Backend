19d64c8b76ff09162cca1d7bcb40a2cae17c3402
lghiggino <lghiggino@gmail.com>
2021-08-26 21:54:15 -0300

    refactor logger util + new tests

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -11,13 +11,25 @@ test("notes are returned as json", async () => {
         .expect("Content-Type", /application\/json/)
 })
 
-test("post one", async () => {
-    const result = await api.post("/api/notes", { "content": "Banana", "date": new Date().toLocaleDateString(), "important": true })
-    // .expect(200)
-    // .expect("Content-Type", /application\/json/)
-    console.log(result)
-    expect(result.content).toBe("Banana")
+test("there are two notes", async () => {
+    const response = await api.get("/api/notes")
+    expect(response.body).toHaveLength(2)
 })
+
+test("the first note is about HTTP methods", async () => {
+    const response = await api.get("/api/notes")
+    expect(response.body[0].content).toBe("HTML is easy")
+})
+
+
+
+// test("post one", async () => {
+//     const result = await api.post("/api/notes", { "content": "Banana", "date": new Date().toLocaleDateString(), "important": true })
+//     // .expect(200)
+//     // .expect("Content-Type", /application\/json/)
+//     console.log(result)
+//     expect(result.content).toBe("Banana")
+// })
 
 // test("note with id is returned properly", async () => {
 //     const result = await api
