a36b1262afeb85d2329d7683a386c88821449c77
lghiggino <lghiggino@gmail.com>
2021-08-30 21:10:34 -0300

    clearing the DB beforeEach test

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -1,8 +1,29 @@
 const mongoose = require("mongoose")
 const supertest = require("supertest")
 const app = require("../app")
-
 const api = supertest(app)
+const Note = require("../models/noteModel")
+
+const initialNotes = [
+    {
+        content: "HTML is easy",
+        date: new Date(),
+        important: false
+    },
+    {
+        content: "Browser can execute only Javascript",
+        date: new Date(),
+        important: true
+    },
+]
+
+beforeEach( async () => {
+    await Note.deleteMany({})
+    let noteObject = new Note(initialNotes[0])
+    await noteObject.save()
+    noteObject = new Note(initialNotes[1])
+    await noteObject.save()
+})
 
 describe("GET", () => {
     it("should return notes as json", async () => {
