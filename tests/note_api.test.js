87733d8ea57f9d26e3c298ed800fc723e0227a50
lghiggino <lghiggino@gmail.com>
2021-09-06 20:19:01 -0300

    updated helper function

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -8,10 +8,10 @@ const Note = require("../models/noteModel")
 
 beforeEach(async () => {
     await Note.deleteMany({})
-    let noteObject = new Note(helper.initialNotes[0])
-    await noteObject.save()
-    noteObject = new Note(helper.initialNotes[1])
-    await noteObject.save()
+
+    const noteObjects = helper.initialNotes.map(note => new Note(note))
+    const promiseArray = noteObjects.map(note => note.save())
+    await Promise.all(promiseArray)
 })
 
 describe("GET", () => {
