0569a957dea6320f36ec11bc102037c1a7ad2500
lghiggino <lghiggino@gmail.com>
2021-09-13 21:56:22 -0300

    user saves note and note saves user in mongo

--- controllers/notesController.js
+++ controllers/notesController.js
@@ -1,5 +1,6 @@
 const notesRouter = require("express").Router()
 const Note = require("../models/noteModel")
+const User = require("../models/userModel")
 
 notesRouter.get("/", async (request, response) => {
     const notes = await Note.find({})
@@ -25,12 +26,17 @@ notesRouter.get("/:id", async (request, response, next) => {
 notesRouter.post("/", async (request, response, next) => {
     const body = request.body
 
+    const user = await User.findById(body.userId)
+
     const note = new Note({
         content: body.content,
         important: body.important || false,
-        date: new Date()
+        date: new Date(),
+        user: user._id
     })
     const savedNote = await note.save()
+    user.notes = user.notes.concat(savedNote._id)
+    await user.save()
     response.json(savedNote)
 
 })
