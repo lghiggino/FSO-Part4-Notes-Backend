e766fc373af06036dde72ff6822f0aebc1f77358
lghiggino <lghiggino@gmail.com>
2022-11-05 11:03:29 -0300

    bugfix model and app mongoose connection doubled

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
-const dotenv = require('./utils/config')
+const config = require('./utils/config')
 const express = require('express')
-const cors = require('cors')
-const { requestLogger, unknownEndpoint, errorHandler } = require('./utils/utils')
-const notesRouter = require('./controllers/notes')
-
-const Note = require('./models/note')
-
 const app = express()
+const cors = require('cors')
+const notesRouter = require('./controllers/notes')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
-app.use(requestLogger)
+app.use(middleware.requestLogger)
 
 app.use('/api/notes', notesRouter)
 
-app.use(unknownEndpoint)
-app.use(errorHandler)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
-module.exports = { app }
+module.exports = app
