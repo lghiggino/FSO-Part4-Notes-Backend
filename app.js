e97f7bfb1f7a7cd4c7fe6327c2a520514e0b9e45
lghiggino <lghiggino@gmail.com>
2022-11-17 08:28:07 -0300

    init jwt + loginRouter

--- app.js
+++ app.js
@@ -1,33 +1,38 @@
-const config = require('./utils/config')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const notesRouter = require('./controllers/notes.controller')
-const usersRouter = require('./controllers/user.controller')
-const middleware = require('./utils/middleware')
-const logger = require('./utils/logger')
-const mongoose = require('mongoose')
+const config = require("./utils/config");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+const notesRouter = require("./controllers/notes.controller");
+const usersRouter = require("./controllers/user.controller");
+const loginRouter = require("./controllers/login.controller");
+
+const middleware = require("./utils/middleware");
+const logger = require("./utils/logger");
+const mongoose = require("mongoose");
 
 
-logger.info('connecting to', config.MONGODB_URI)
+logger.info("connecting to", config.MONGODB_URI);
 
-mongoose.connect(config.MONGODB_URI)
+mongoose
+  .connect(config.MONGODB_URI)
   .then(() => {
-    logger.info('connected to MongoDB')
+    logger.info("connected to MongoDB");
   })
   .catch((error) => {
-    logger.error('error connecting to MongoDB:', error.message)
-  })
+    logger.error("error connecting to MongoDB:", error.message);
+  });
 
-app.use(cors())
-app.use(express.static('build'))
-app.use(express.json())
-app.use(middleware.requestLogger)
+app.use(cors());
+app.use(express.static("build"));
+app.use(express.json());
+app.use(middleware.requestLogger);
 
-app.use('/api/notes', notesRouter)
-app.use('/api/users', usersRouter)
+app.use("/api/notes", notesRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
-module.exports = app
+module.exports = app;
--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
 Project built with express and react
 
-//https://fullstackopen.com/en/part4/user_administration#creating-users
+//https://fullstackopen.com/en/part4/token_authentication#limiting-creating-new-notes-to-logged-in-users
